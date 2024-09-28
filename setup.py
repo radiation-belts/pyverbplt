d00ef0edf547464184fc5a64a158d94d4b3f39f2
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2024-09-28 04:19:09 -0700

    Fixing PyPI setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyverbplt',
-    version='24.08',
+    version='24.09',
     author='Alexander Drozdov',
     author_email='adrozdov@ucla.edu',
     description='Python library designed to load `.plt` files, created by the Versatile Electron Radiation Belts (VERB) code',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/radiation-belts/pyverbplt',
-    packages=find_packages(),
+    py_modules=['pyverbplt'],  # Use py_modules for single-file modules
     install_requires=open('requirements.txt').read().splitlines(),
     classifiers=[
         'Programming Language :: Python :: 3',
